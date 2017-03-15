f763996f26082d4e320b45845601d5a96b3fe5e2
Savannah Lokey <savannah.lokey@gmail.com>
2017-03-15 10:36:08 -0400

    add a comment on data download

--- metasearch_analysis.py
+++ metasearch_analysis.py
@@ -1,1 +1,1 @@
-# coding: utf-8#get_ipython().system('git clone http://github.com/OpenNeuroLab/metasearch.git')x = 5
+# coding: utf-8#Download our data from openneu.ro#get_ipython().system('git clone http://github.com/OpenNeuroLab/metasearch.git')x = 5
