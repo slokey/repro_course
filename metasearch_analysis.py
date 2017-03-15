cb569905e7773ddf56a34b8f9a118f9a6871163d
Savannah Lokey <savannah.lokey@gmail.com>
2017-03-15 11:29:28 -0400

    move metasearch into untracked directory

--- metasearch_analysis.py
+++ metasearch_analysis.py
@@ -1,1 +1,3 @@
 # coding: utf-8#Download our data from openneu.ro#get_ipython().system('git clone http://github.com/OpenNeuroLab/metasearch.git')x = 5
+Path('data_not_in_repo').mkdir()
+get_ipython().magic('mv metasearch/ data_not_in_repo/')
